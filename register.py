1a60b4fa3189ca3c02466bf0c29c00f97292c612
José Eduardo <jose.edu.vs516@gmail.com>
2024-04-16 19:15:52 -0300

    Full tabs register

--- register.py
+++ register.py
@@ -1,6 +1,5 @@
 import openpyxl as xl
 
-
 teams = ('Acre', 'Amazonense', 'C.F.C', 'Floresta', 'Rural', 'Silvestre')
 teams_data = dict()
 for c in teams:
@@ -9,7 +8,7 @@ for c in teams:
 file = xl.load_workbook('Era Fm.xlsx')
 sheet = file.active = file['Retrospecto times']
 
-year = input('Diga o ano para registrar: ')
+year = input('Digite o ano de registro: ')
 
 for cell in sheet[1]:
     if year == str(cell.value):
@@ -50,4 +49,33 @@ for line in range(3, 9):
         except:
             continue
 
-file.save('Era Fm Copy.xlsx')
+for line in range(12, 18):
+    team = sheet[f'A{line}'].value
+    
+    if teams_data[team]['Copa do Brasil'] not in ('1ª fase', '2ª fase'):
+        sheet[f'G{line}'] = sheet[f'G{line}'].value + 1
+    
+    if teams_data[team]['Copa Verde'] != 'F.G':
+        sheet[f'H{line}'] = sheet[f'H{line}'].value + 1
+    
+    if teams_data[team]['Brasileirão'] != '-':
+        br = teams_data[team]['Brasileirão'][teams_data[team]['Brasileirão'].find('[')+1]
+    
+    try:
+        if br == 'A':
+            sheet[f'I{line}'] = sheet[f'I{line}'].value + 1
+        elif br == 'B':
+            sheet[f'J{line}'] = sheet[f'J{line}'].value + 1
+        elif br == 'C':
+            sheet[f'K{line}'] = sheet[f'K{line}'].value + 1
+    except:
+        sheet[f'L{line}'] = sheet[f'L{line}'].value + 1
+
+    if teams_data[team]['Liberta.'] != '-':
+        sheet[f'E{line}'] = sheet[f'E{line}'].value + 1
+    
+    if teams_data[team]['Sula.'] != '-':
+        sheet[f'F{line}'] = sheet[f'F{line}'].value + 1
+
+file.save('Era Fm.xlsx')
+file.close()
